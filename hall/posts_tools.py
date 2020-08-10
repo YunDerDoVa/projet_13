d7cb9df2d2712b0576df5b5fa3d421b4db4f6d49
ArthurNeyer <arthur.neyer@protonmail.com>
2020-08-10 17:46:53 +0200

    Comment posts_tools

--- hall/posts_tools.py
+++ hall/posts_tools.py
@@ -2,9 +2,14 @@ from table.models import TablePost
 
 
 class DiscoverTool:
+    """ This package is a suite of tools to interact with the
+    table django app. """
 
     def __init__(self, *models):
         self.models = models
 
     def get_posts(self):
+        """ This method return TablePost queryset. Order it randomly
+        and reorder it with a simple algorithm. """
+
         return TablePost.objects.all()
