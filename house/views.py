077659be77010b12b9b3a0229d9abf62bb2eaa11
ArthurNeyer <arthur.neyer@protonmail.com>
2020-08-13 20:40:54 +0200

    bases de l'app house

--- house/views.py
+++ house/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 
 
+from table.models import TablePost, TableLike
+
+
 # Create your views here.
 def dashboard(request):
     return render(request, 'dashboard.html.django')
 
 
 def my_library(request):
-    return render(request, 'my_library.html.django')
+
+    posts = TablePost.objects.filter(user=request.user).all()
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'my_library.html.django', context)
 
 
 def my_likes(request):
-    return render(request, 'my_likes.html.django')
+
+    likes = TableLike.objects.filter(like_from=request.user).all()
+
+    context = {
+        'likes': likes,
+    }
+
+    return render(request, 'my_likes.html.django', context)
