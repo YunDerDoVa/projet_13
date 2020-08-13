8fbb217dfce637438c52833d83f8c6956e7bafb1
ArthurNeyer <arthur.neyer@protonmail.com>
2020-08-13 20:14:07 +0200

    bases de l'app table

--- table/views.py
+++ table/views.py
@@ -19,14 +19,15 @@ def publish(request):
 
     table_post = TablePost(user=request.user)
 
-    form = PostForm()
+    form = PostForm(instance=table_post)
 
     if request.method == 'POST':
         form = PostForm(request.POST)
 
         if form.is_valid():
             table_post = form.save()
-            return redirect('post', post_id=table_post)
+
+            return redirect('post', post_id=table_post.id)
 
     context = {
         'form': form,
@@ -37,10 +38,30 @@ def publish(request):
 
 
 def edit(request, post_id):
-    table_post = TablePost.objects.get(id=post_id)
+
+    table_post = TablePost.objects.get(id=post_id, user=request.user)
+
+    form = PostForm(instance=table_post)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+            table_post.title = cd['title']
+            table_post.description = cd['description']
+            table_post.readme = cd['readme']
+            table_post.script_js = cd['script_js']
+            table_post.style_css = cd['style_css']
+            table_post.private_psot = cd['private_post']
+
+            table_post.save()
+
+            return redirect('post', post_id=post_id)
 
     context = {
-        'post': table_post,
+        'form': form,
+        'background_instructions': table_post.get_background_instructions()
     }
 
     return render(request, 'table/edit_post.html.django', context)
