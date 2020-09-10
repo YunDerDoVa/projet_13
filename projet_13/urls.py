cb1b026ea8f98b2f9dd420f36e821380f36ead58
ArthurNeyer <arthur.neyer@protonmail.com>
2020-09-10 15:56:43 +0200

    Update urls.py

--- projet_13/urls.py
+++ projet_13/urls.py
@@ -28,11 +28,11 @@ urlpatterns = [
     path('color/', include('color_tool.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
     urlpatterns = [
